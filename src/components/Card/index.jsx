3cf994a18f3245d3d79565ede4fcf8fa952a9ac1
ashan <ashannon1990@gmail.com>
2020-01-12 20:25:42 -0500

    client directory

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './card.css'
 
+
 const Card = (props) => {
+
     return (
         <div className="card">
             <div className="cardHeader">
@@ -9,7 +11,7 @@ const Card = (props) => {
                 <i className="fa fa-ellipsis-h" />
             </div>
             <div>
-                <img className="photo" src={props.imageURL} alt="image" />
+                <img className="photo" src={props.imageURL} alt="userImage" />
             </div>
             <div className="cardIcons icons">
                 <i className="fa fa-heart" />
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
-import './navbar.css'
+import './navbar.css';
+
+const styles = {
+    topNav: {
+        height: '56px',
+        width: '100%',
+        position: 'fixed',
+        backgroundColor: 'white',
+        top: 0,
+        left: 0,
+        display: 'flex',
+        alignItems: 'center',
+        padding: '0 20px',
+        boxSizing: 'border-box',
+        zIndex: '90',
+        fontSize: '25px',
+        borderBottom: 'black solid 1px',
+        marginBottom: '20px',
+    },
+    container: {
+        width: '100%',
+        maxWidth: '960px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: 'auto'
+    },
+    i: {
+        margin: '10px',
+    }
+}
 
 const Navbar = () => {
+
     return (
-        <div className="TopNav">
-            <div className="container">
+        <div style={styles.topNav}>
+            <div style={styles.container}>
                 <div>
                     <i className="fa fa-instagram" />
                     <span className="insta">Instagram</span>
